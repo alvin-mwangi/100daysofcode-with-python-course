6a696bfea618ae350b5df1ae20689c66188e780c
alvin-mwangi <alvin.mwangi@gmail.com>
2019-04-01 13:54:03 -0700

    updates from classes chapter

--- exercises/hello.py
+++ exercises/hello.py
@@ -1,3 +1,8 @@
-def hello_name(name):
-    return f'hello {name}'
+# def hello_name(name):
+#     return f'hello {name}'
 
+testLine = "this is line3"
+if "line3" in testLine:
+    print("true")
+else:
+    print("false")
