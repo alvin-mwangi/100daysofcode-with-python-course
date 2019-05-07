34be13bbd065ea7662585a9a24b55c6652ec4dd8
alvin-mwangi <alvin.mwangi@gmail.com>
2019-05-07 16:31:37 -0700

    measuring perf updates

--- days/37-39-csv-data-analysis/practice/csvParsingExercise/test.py
+++ days/37-39-csv-data-analysis/practice/csvParsingExercise/test.py
@@ -1,14 +1,18 @@
 
-import collections
-from typing import List
+# import collections
+# from typing import List
 
 
-Record = collections.namedtuple(
-    'Record',
-    'RespondentID,USRegion,HouseholdIncome,MainDish'
-)
+# Record = collections.namedtuple(
+#     'Record',
+#     'RespondentID,USRegion,HouseholdIncome,MainDish'
+# )
 
 
-Record.RespondentID = 'test123'
+# Record.RespondentID = 'test123'
 
-print(getattr(Record, 'RespondentID'))
+# print(getattr(Record, 'RespondentID'))
+
+for i in range(1,11):
+    if(i % 2 == 0):
+        print(i)
