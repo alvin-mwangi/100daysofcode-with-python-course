d160611d16fb2f4e4f7cb3a963d7410e51dedc1a
alvin-mwangi <alvin.mwangi@gmail.com>
2019-05-10 09:03:49 -0700

    tempdict approach -- not working

--- exercises/scratch3_longest_substring.py
+++ exercises/scratch3_longest_substring.py
@@ -34,6 +34,32 @@ Explanation: The answer is "wke", with the length of 3.
 # use dictionary to keep track of letters we've seen
 # if letter exists, reset counter (create a new island)
 # otherwise, increment the counter 
+# issues: doesn't work if larger island/substring can be found starting from middle of one of the islands
+# example: dvdf: islands: 'dv' and 'df'
+# however, larger island/substring possible starting from 'v' 
+# (within first island): 'd' and 'vdf'
+# how to check this? 
+# solutions: 
+#     find substrings of max length and work backward? 
+#        --- doesn't work if substring is at start of string 
+#         -- e.g., 'anviaj' => 'anvi' 
+#     sliding window approach?
+#         - check susbtrings from start of string
+#         - if string length > currentMax
+# examples:
+# input: 'anviaj'
+#  1: anviaj' => 'anvi' 'aj', currentMax: 4
+#  2: stringLength(5) > currentMax (4) 
+#     'nviaj' => 'nviaj', currentMax: 5
+#  3: stringLength(5) == currentMax --> stop here and output the currentMax
+#         
+# 
+# input: 'dvdf'
+# 1: dvdf => 'dv', 'df', currentMax: 2
+# 2: stringLength(4) > currentMax(2)
+#    'vdf' => 'vdf', currentMax(3)
+# 3: stringLength(3) == currentMax --> stop here and output the currentMax
+#         
 
 
 '''
@@ -79,33 +105,50 @@ class Solution:
         substringLength = 0
         
         if(len(s) > 0):
-            for i in s:
-                if letterDict.get(i) == None:
-                    letterDict[i] = 1
+            for i in range(0, len(s)):
+                if letterDict.get(s[i]) == None:
+                    letterDict[s[i]] = 1
                     substringLength += 1
-                    substringList.append([i, substringLength])
+                    substringList.append([s[i], substringLength, i])
 
                 # else case here
                 else:
-                    letterDict[i] += 1
+                    letterDict[s[i]] += 1
                     substringLength = 1
-                    substringList.append([i, substringLength])
+                    substringList.append([s[i], substringLength, i])
             
                        
             print(letterDict)
             print(substringList)
 
             # print results
-            maxLen = max(j for i,j in substringList)
-            print(maxLen)
+            maxLen = max(j for i,j,k in substringList)
+            #print(maxLen)
             #return max(j for i,j in substringList) 
+
+            maxSubstringEndIndexes = [k for i,j,k in substringList if j == maxLen and k > j]
+            
+            if(len(maxSubstringEndIndexes) > 0):
+                for endIndex in maxSubstringEndIndexes:
+                    tempDict = dict()
+                    startIndex = endIndex - maxLen + 1
+                    for i in range(startIndex, endIndex + 1):
+                        print(f"adding '{s[i]} to tempDict...")
+                        tempDict[s[i]] = 1
+                    for i in range(startIndex - 1, 0, -1):
+                        if(tempDict.get(s[i]) == None):
+                            maxLen += 1
+            #print(maxLen)
+            return maxLen
+                        
         else:
             return 0
 
-if __name__ == "__main__":        
+if __name__ == "__main__":    
+    testString = "anviaj"
     sol = Solution()
-    sol.lengthOfLongestSubstring("dvdf")
+    sol.lengthOfLongestSubstring(testString)
 
 
-    
+
 
