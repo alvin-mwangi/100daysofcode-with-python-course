4e9fa0fd655d6b50e209239a0b9d959edeae85fd
alvin-mwangi <alvin.mwangi@gmail.com>
2019-05-15 15:19:40 -0700

    working initial version

---
+++ days/55-57-uplink/moviesapi/api.py
@@ -0,0 +1,21 @@
+
+import uplink
+import requests
+import datetime
+
+class MovieSearchClient(uplink.Consumer):
+    def __init__(self):
+        super().__init__(base_url='http://movie_service.talkpython.fm')
+
+    @uplink.get('/api/search/{keyword}')
+    def search_by_keyword(self, keyword) -> requests.models.Response:
+        """ Gets movies by keyword """
+    
+    @uplink.get('/api/director/{director_name}')
+    def search_by_director(self, director_name) -> requests.models.Response:
+        """ Gets movies by director name """
+    
+    @uplink.get('/api/movie/{imdb_id}')
+    def search_by_imdb_number(self, imdb_id) -> requests.models.Response:
+        """ Gets movies by imdb id """
+
